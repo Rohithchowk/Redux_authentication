0b244f512099e34519953bafeb9ca7aa8a1406ad
Rohith Chowki <119583656+Rohithchowk@users.noreply.github.com>
2025-01-31 19:55:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const router = require("./routes/users");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 //! Connect to mongodb
-mongoose
-  .connect("mongodb+srv://sriraman895:sriram895@cluster0.xnrax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-  .then(() => console.log("Db connected successfully"))
-  .catch((e) => console.log(e));
+mongoose.connect('mongodb+srv://sriraman895:sriram895@cluster0.xnrax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log('Error connecting to MongoDB:', err));
 
 //! Middlewares
 app.use(express.json()); //pass incoming json data from the user
