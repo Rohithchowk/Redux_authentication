764858df2c4dac440cf39603035bb5e4e9ca0138
Rohith Chowki <119583656+Rohithchowk@users.noreply.github.com>
2025-01-31 19:54:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 //! Connect to mongodb
 mongoose
-  .connect("mongodb://localhost:27017/auth-api")
+  .connect("mongodb+srv://sriraman895:sriram895@cluster0.xnrax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("Db connected successfully"))
   .catch((e) => console.log(e));
 
